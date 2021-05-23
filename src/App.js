7996ab06d8d39cfe1c5021243229068199522a78
Aashiish-Pandey <ash006pandey@gmail.com>
2021-05-24 03:06:41 +0530

    Pagination final

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import Posts from './components/Posts'
+import React, { useState, useEffect } from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+import axios from 'axios';
+import './App.css';
 
 const App = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
+  const [postsPerPage] = useState(10);
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => {
-        setPosts(response.data)
-        
-      });
-    setLoading(false);
-    
-  },[]);
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    };
 
-  // Get Current Post
-   const indexOfLastPost = currentPage*postsPerPage;
-   const indexOfFirstPost = indexOfLastPost-postsPerPage;
-   const currentPosts = posts.slice(indexOfFirstPost,indexOfLastPost);
+    fetchPosts();
+  }, []);
+
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
-    
-    <div className ='container mt-5'>
-      <h1 className ='text-primary mb-3'>My Blog</h1>
-      <Posts posts ={currentPosts} loading ={loading}/>
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
-  )
+  );
 };
 
 export default App;
